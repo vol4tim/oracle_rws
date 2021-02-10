ce752adaae8b989b89e2f890e5d8c034ead66896
vol4tim <vol4tim@gmail.com>
2021-02-10 16:24:58 +0400

    reconnect infura

--- src/services/eth_chain.js
+++ src/services/eth_chain.js
@@ -3,7 +3,7 @@ import config from "../../config.json";
 import ABI from "../../abi/Subscribetion.json";
 
 export const web3 = new Web3(
-  new Web3.providers.WebsocketProvider(config.ETH_CHAIN_API)
+  new Web3.providers.WebsocketProvider(config.ETH_CHAIN_API, config.ETH_OPTIONS)
 );
 
 export const contract = new web3.eth.Contract(ABI, config.CONTRACT);
